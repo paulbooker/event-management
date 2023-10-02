4a6fe570b2ee4eb0f4d41d3b939eb9fbd3506409
Paul Booker <paul@paulbooker.co.uk>
2023-10-02 15:12:56 +0100

    udemy-37245992-65

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('events', EventContoller::class);
+Route::apiResource('events.attendees', AttendeeController::class)->scoped(['attendee' => 'event']);

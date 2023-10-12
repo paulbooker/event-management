805c4ab0c043f96eb0a8b63352d07dbc73efb5b5
Paul Booker <paul@paulbooker.co.uk>
2023-10-12 16:44:23 +0100

    udemy-37245992-81

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -7,12 +7,18 @@ use App\Http\Resources\EventResource;
 use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class EventController extends Controller
 {
 	use CanLoadRelationships;
 	
 	private array $relations = ['user', 'attendees', 'attendees.user'];  // PHP8: readonly
+	
+	public function __construct() {
+		$this->middleware('auth:sanctum')->except(['index', 'show']);
+		$this->authorizeResource(Event::class, 'event');
+	}
 	
     /**
      * Display a listing of the resource.
@@ -38,7 +44,7 @@ class EventController extends Controller
 				'start_time' => 'required|date',
 				'end_time' => 'required|date|after:start_time'	   
 			]),
-			'user_id' => 1   
+			'user_id' => $request->user()->id   
         ]);
         
         return new EventResource($this->loadRelationships($event));
@@ -57,6 +63,11 @@ class EventController extends Controller
      */
     public function update(Request $request, Event $event)
     {
+	    //if (Gate::denies('update-event', $event)) {
+		//    abort(403, 'You are not authorized to update this event');
+	    //}
+	    //$this->authorize('update-event', $event);
+	    
 	    $event->update(
 		    $request->validate([
 		    	'name' => 'sometimes|string|max:255',
